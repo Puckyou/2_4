4fbc9aff7085dd719cc34cb05ecc3525468c8488
LENOVO-PC\user <snobl@bk.ru>
2017-06-01 18:40:05 +0800

    123

---
+++ find_procedure.py
@@ -0,0 +1,67 @@
+
+# заготовка для домашней работы
+# прочитайте про glob.glob
+# https://docs.python.org/3/library/glob.html
+
+# Задание
+# мне нужно отыскать файл среди десятков других
+# я знаю некоторые части этого файла (на память или из другого источника)
+# я ищу только среди .sql файлов
+# 1. программа ожидает строку, которую будет искать (input())
+# после того, как строка введена, программа ищет её во всех файлах
+# выводит список найденных файлов построчно
+# выводит количество найденных файлов
+# 2. снова ожидает ввод
+# поиск происходит только среди найденных на этапе 1
+# 3. снова ожидает ввод
+# ...
+# Выход из программы программировать не нужно.
+# Достаточно принудительно остановить, для этого можете нажать Ctrl + C
+
+# Пример на настоящих данных
+
+# python3 find_procedure.py
+# Введите строку: INSERT
+# ... большой список файлов ...
+# Всего: 301
+# Введите строку: APPLICATION_SETUP
+# ... большой список файлов ...
+# Всего: 26
+# Введите строку: A400M
+# ... большой список файлов ...
+# Всего: 17
+# Введите строку: 0.0
+# Migrations/000_PSE_Application_setup.sql
+# Migrations/100_1-32_PSE_Application_setup.sql
+# Всего: 2
+# Введите строку: 2.0
+# Migrations/000_PSE_Application_setup.sql
+# Всего: 1
+
+# не забываем организовывать собственный код в функции
+# на зачёт с отличием, использовать папку 'Advanced Migrations'
+
+import glob
+import os.path
+def get_files_list():
+	migrations = 'Migrations'
+	files = glob.glob(os.path.join(migrations, "*.sql"))
+	return files
+def ask_the_loop(files):
+	files = get_files_list()
+	while True:
+		my_input = input("Введите фрагмент файла:")
+		my_file_list = []
+		for my_file in files:
+			with open(my_file, "r") as f:
+				lines = f.readlines()
+				for line in lines:
+					if my_input in line:
+						my_file_list.append(my_file)
+						break
+		files = my_file_list
+		for x in files:
+			print("{}\n".format(x))
+		print("Всего файлов:{}".format(len(files)))
+
+ask_the_loop(get_files_list())
